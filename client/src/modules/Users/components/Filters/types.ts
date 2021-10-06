1f7a354ba2b3c2c4b4d4dfa857b75269c9360726
Gggarin <nikitingggarin@gmail.com>
2021-10-06 16:58:43 +0300

    filterRes0

---
+++ client/src/modules/Users/components/Filters/types.ts
@@ -0,0 +1,11 @@
+export interface IProfile {
+  id?: string,
+  firstname?: string,
+  lastname?: string,
+  email: string,
+  password?: string,
+  description?: string,
+  isMentor?: boolean,
+  careerStart?: string,
+  company?: string,
+}
