e2684c4add930e6be08c6d84fe6f9878e18ef420
Юлия Глухова <juliaglukhova666@gmail.com>
2021-10-10 23:11:59 +0400

    meet manager finally done

--- client/src/modules/Profile/components/Manager/components/MeetCardTab.tsx
+++ client/src/modules/Profile/components/Manager/components/MeetCardTab.tsx
@@ -3,9 +3,9 @@ import { Card } from 'antd';
 import { IMeetCard } from './types';
 
 const MeetCard: React.FC<IMeetCard> = ({ buttons, meetData }) => (
-  <Card type="inner" title="Тема: Работа в Озон" extra={buttons} style={{ margin: '30px 0' }}>
+  <Card type="inner" title={`От: ${meetData.firstname} ${meetData.lastname}`} extra={buttons} style={{ margin: '30px 0' }}>
     <p>Дата: 12.10.2021</p>
-    <p>{`${meetData.firstname} ${meetData.lastname}`}</p>
+    <p>{`${meetData.comment}`}</p>
   </Card>
 );
 
