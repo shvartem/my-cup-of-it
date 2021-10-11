5deb6439873bc7c05483d0cfc2409e0729beab31
shvartem <shvartem422@yandex.ru>
2021-10-11 17:30:53 +0500

    adminCabinet is done

--- client/src/redux/sagas/technologies.ts
+++ client/src/redux/sagas/technologies.ts
@@ -1,7 +1,14 @@
 import { SagaIterator } from 'redux-saga';
 import { call, put, takeEvery } from 'redux-saga/effects';
-import { ITechnology } from '../../types/technologiesTypes';
-import { getData } from '../tools';
+import {
+  IAddNewTechnologyAction,
+  IDeleteTechnologyAction,
+  IEditTechnologyAction,
+  ITechnology,
+} from '../../types/technologiesTypes';
+import {
+  deleteData, editData, getData, postData,
+} from '../tools';
 import { actions } from '../slices/index';
 
 function* getAllTechnologies(): SagaIterator {
@@ -13,6 +20,36 @@ function* getAllTechnologies(): SagaIterator {
   }
 }
 
+function* addNewTechnology({ payload }: IAddNewTechnologyAction): SagaIterator {
+  try {
+    const newTechnology = yield call(() => postData<ITechnology>('/api/technologies', payload));
+    yield put(actions.addNewTechnologyFulfilled(newTechnology));
+  } catch (e) {
+    yield put(actions.addNewTechnologyRejected(e as string));
+  }
+}
+
+function* editTechnology({ payload }: IEditTechnologyAction): SagaIterator {
+  try {
+    const technology = yield call(() => editData<ITechnology>('/api/technologies', payload));
+    yield put(actions.editTechnologyFulfilled(technology));
+  } catch (e) {
+    yield put(actions.editTechnologyRejected(e as string));
+  }
+}
+
+function* deleteTechnology({ payload }: IDeleteTechnologyAction): SagaIterator {
+  try {
+    yield call(() => deleteData('/api/technologies', payload.id));
+    yield put(actions.deleteTechnologyFulfilled(payload));
+  } catch (e) {
+    yield put(actions.deleteTechnologyRejected(e as string));
+  }
+}
+
 export default function* allTechnologiesSaga() {
   yield takeEvery(actions.getAllTechnologiesPending, getAllTechnologies);
+  yield takeEvery(actions.addNewTechnologyPending, addNewTechnology);
+  yield takeEvery(actions.editTechnologyPending, editTechnology);
+  yield takeEvery(actions.deleteTechnologyPending, deleteTechnology);
 }
--- server/db/models/company.js
+++ server/db/models/company.js
@@ -21,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     },
   }, {
     sequelize,
+    timestamps: false,
     modelName: 'Company',
   });
   return Company;
