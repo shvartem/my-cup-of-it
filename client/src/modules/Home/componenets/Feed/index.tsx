1c04a60733ce83e359c417785121555839315268
Юлия Глухова <juliaglukhova666@gmail.com>
2021-10-11 19:18:25 +0400

    some design

--- client/src/modules/Home/componenets/Feed/index.tsx
+++ client/src/modules/Home/componenets/Feed/index.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
+import { Button } from 'antd';
 import UserCard from '../UserCard';
 import { useAppSelector } from '../../../../hooks';
 import Container from '../style';
 import Spinner from '../../../common/Spinner';
 
 const CardsWrapper = styled.div`
-display: flex;
-justify-content: center;
-flex-wrap: wrap;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
 const Feed: React.FC = () => {
   const users = useAppSelector((state) => state.allUsers.data);
@@ -21,17 +22,16 @@ const Feed: React.FC = () => {
   }
   return (
     <Container>
-      <h1>Наши профecсионалы</h1>
+      <h1>Часть наших профecсионалов</h1>
       <CardsWrapper>
         <UserCard mentor={users[0]} />
         <UserCard mentor={users[0]} />
         <UserCard mentor={users[0]} />
         <UserCard mentor={users[0]} />
-        <UserCard mentor={users[0]} />
-        <UserCard mentor={users[0]} />
-        <UserCard mentor={users[0]} />
-        <UserCard mentor={users[0]} />
       </CardsWrapper>
+      <div style={{ textAlign: 'center', margin: '15px 0' }}>
+        <Button>Посмотреть еще</Button>
+      </div>
     </Container>
   );
 };
